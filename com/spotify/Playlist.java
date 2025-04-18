e611b12378773b09ee01a8701b27a511f5237bed
Hashem Nickravan <hashem.nickravan@gmail.com>
2025-04-18 20:07:44 +0330

    Complete playlist class

--- com/spotify/Playlist.java
+++ com/spotify/Playlist.java
@@ -5,4 +5,30 @@ public class Playlist {
     private String title;
     private User owner;
     private ArrayList<Music> playlist = new ArrayList<>();
+
+    public Playlist(String title, User owner) {
+        this.title = title;
+        this.owner = owner;
+    }
+
+    public void editTitle(String newTitle, String password) {
+        if(!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Invalid password");
+        this.title = newTitle;
+    }
+
+    public void addMusic(Music music, String password) {
+        if(!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Invalid password");
+        if(playlist.contains(music))
+            throw new InvalidOperationException("Music is in playlist of before");
+        playlist.add(music);
+    }
+
+    public void removeMusic(Music music, String password) {
+        if(!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Invalid password");
+        if(!playlist.remove(music))
+            throw new InvalidOperationException("Music not exist in playlist");
+    }
 }
