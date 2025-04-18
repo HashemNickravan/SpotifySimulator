67aac9bd03f022b798588ca03f1ea427205c5ed2
Hashem Nickravan <hashem.nickravan@gmail.com>
2025-04-18 19:38:10 +0330

    Add Music and Playlist classes

---
+++ com/spotify/Music.java
@@ -0,0 +1,8 @@
+package com.spotify;
+import java.util.ArrayList;
+
+public class Music {
+    private String title;
+    private User singer;
+    private static ArrayList<Music> allMusics = new ArrayList<>();
+}
---
+++ com/spotify/Playlist.java
@@ -0,0 +1,8 @@
+package com.spotify;
+import java.util.ArrayList;
+
+public class Playlist {
+    private String title;
+    private User owner;
+    private ArrayList<Music> playlist = new ArrayList<>();
+}
