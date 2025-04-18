a8b8047bf5cb42507241fd7be33981332e4244d9
Hashem Nickravan <hashem.nickravan@gmail.com>
2025-04-18 19:08:03 +0330

    Add UserBehavior interface

---
+++ com/spotify/UserBehavior.java
@@ -0,0 +1,7 @@
+package com.spotify;
+
+public interface UserBehavior {
+    void createPlaylist(String title, User owner);
+    void playMusic(Music music);
+    void buyPremium(User owner, int month);
+}
